559d6c87f49496967e9620ea7c0076dddec17895
kensin9470 <kensin9470@naver.com>
2015-11-11 16:22:49 +0900

    Update HelloTest.java
    github 입니다.

--- test1111/src/test1111/HelloTest.java
+++ test1111/src/test1111/HelloTest.java
@@ -5,6 +5,7 @@ public class HelloTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("master 입니다.");
+		System.out.println("github 입니다.");
 	}
 
 }
